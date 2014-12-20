d11a2677db6fc1ae1755e7fa6173c95192f7afe7
gregfedirko <gfedirko@zagmail.gonzaga.edu>
2014-12-19 21:14:39 -0800

    static site

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
 var express = require('express');
-
+var stylus = require('stylus');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 var app = express();
 
+function compile(str, path) {
+  return stylus(str).set('filename', path);
+}
+
 app.set('views', __dirname + '/server/views');
 app.set('view engine', 'jade');
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(stylus.middleware(
+  {
+    src: __dirname + '/public', 
+    compile: compile
+  }
+));
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('*', function(req, res) {
   res.render('index');
