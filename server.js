0dfdd3655713ad84eca1b8937c5da2d8ff6db75d
gregfedirko <gfedirko@zagmail.gonzaga.edu>
2014-12-19 22:09:17 -0800

    skelleton complete

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var stylus = require('stylus');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
 var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
@@ -24,12 +25,31 @@ app.use(stylus.middleware(
 
 app.use(express.static(__dirname + '/public'));
 
+mongoose.connect('mongodb://localhost/multivision');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error...'));
+db.once('open', function callback() {
+  console.log('multivision db opened');
+});
+
+var messageSchema = mongoose.Schema({
+  message: String
+});
+
+var Message = mongoose.model('Message', messageSchema);
+var mongoMessage;
+Message.findOne().exec(function(err, messageDoc) {
+  mongoMessage = messageDoc.message;
+});
+
 app.get('/partials/:partialPath', function(req, res) {
   res.render('partials/' + req.params.partialPath);
 });
 
 app.get('*', function(req, res) {
-  res.render('index');
+  res.render('index', {
+    mongoMessage: mongoMessage
+  });
 });
 
 var port = 3030;
