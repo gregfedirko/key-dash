d018aaa4e1cddbc57f3c3d19f60538785f6622d8
gregfedirko <gfedirko@zagmail.gonzaga.edu>
2014-12-20 18:42:58 -0800

    add password authentication

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var User = mongoose.model('User');
 passport.use(new LocalStrategy(
   function(username, password, done) {
     User.findOne({username: username}).exec(function(err, user) {
-      if(user) {
+      if(user && user.authenticate(password)) {
         return done(null, user);
       } else {
         return done(null, false);
--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var crypto = require('crypto');
 
 
 module.exports = function(config) {
@@ -14,16 +15,35 @@ module.exports = function(config) {
   var userSchema = mongoose.Schema({
     firstName: String, 
     lastName: String, 
-    username: String
+    username: String,
+    salt: String, 
+    hashed_pwd: String
   });
+
+  userSchema.methods = {
+    authenticate: function(passwordToMatch) {
+      return hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+    }
+  }
 
   var User = mongoose.model('User', userSchema);
 
   User.find({}).exec(function(err, collection) {
     if (collection.length === 0) {
-      User.create({firstName: 'Greg', lastName: 'Fedirko', username: 'gfedirko'});
-      User.create({firstName: 'Joe', lastName: 'Eames', username: 'joe'});
-      User.create({firstName: 'John', lastName: 'Papa', username: 'jpapa'});
+      var salt, hash;
+      salt = createSalt();
+      hash = hashPwd(salt, 'gfedirko')
+      User.create({firstName: 'Greg', lastName: 'Fedirko', username: 'gfedirko', salt: salt, hashed_pwd: hash});
     }
-  })
+  });
+
 };
+
+function createSalt() {
+  return crypto.randomBytes(128).toString('base64');
+}
+
+function hashPwd(salt, pwd) {
+  var hmac = crypto.createHmac('sha1', salt);
+  return hmac.update(pwd).digest('hex');
+}
