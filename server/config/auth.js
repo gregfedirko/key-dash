cedfe3b2967d47ef74bafdeac3f2ce9e51f26b1f
gregfedirko <gfedirko@zagmail.gonzaga.edu>
2014-12-20 18:04:49 -0800

    refactor server route authentication into module

---
+++ server/config/auth.js
@@ -0,0 +1,14 @@
+var passport = require('passport');
+
+module.exports.authenticate = function(req, res, next) {
+  var auth = passport.authenticate('local', function(err, user) {
+    if (err) {return next(err);}
+    if (!user) {res.send({success: false});}
+    req.logIn(user, function(err) {
+      if (err) {return next(err);}
+      res.send({success: true, user: user});
+    });
+  })
+
+  auth(req, res, next);
+}
--- server/config/routes.js
+++ server/config/routes.js
@@ -1,22 +1,11 @@
-var passport = require('passport');
+var auth = require('./auth');
 
 module.exports = function(app) {
   app.get('/partials/*', function(req, res) {
     res.render('../../public/app/' + req.params[0]);
   });
 
-  app.post('/login', function(req, res, next) {
-    var auth = passport.authenticate('local', function(err, user) {
-      if (err) {return next(err);}
-      if (!user) {res.send({success: false});}
-      req.logIn(user, function(err) {
-        if (err) {return next(err);}
-        res.send({success: true, user: user});
-      });
-    })
-
-    auth(req, res, next);
-  });
+  app.post('/login', auth.authenticate);
 
   app.get('*', function(req, res) {
     res.render('index');
