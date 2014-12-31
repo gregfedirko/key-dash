a54194d3b6a09a07a799432e5659d65f3b727910
gregfedirko <gfedirko@zagmail.gonzaga.edu>
2014-12-30 18:58:27 -0800

    try now link and key press

--- public/app/main/mvMainController.js
+++ public/app/main/mvMainController.js
@@ -1,10 +1,20 @@
-angular.module('app').controller('mvMainController', function($scope) {
+angular.module('app').controller('mvMainController', function($scope, $window, $location, $routeParams) {
 
 
   $scope.labels = [1,2,3,4,5,6,7];
   $scope.series = ['wpm', 'error', 'wasted keys'];
 
   $scope.data = null;
+
+  angular.element($window).on('keypress', function(ev) {
+    if (ev.keyCode === 13) {
+      console.log('foo');
+      $location.path('/exercises');
+      if(!$scope.$$phase) $scope.$apply()
+    }
+  });
+
+
 
   $scope.triggerFunction = function() {
     if (!$scope.data) {
