94b22c5489437a14c4a8e19aac2bcf0507cbfef2
gregfedirko <gfedirko@zagmail.gonzaga.edu>
2015-01-01 22:25:21 -0800

    john papa refactor exercise

--- public/app/exercises/mvExerciseDataService.js
+++ public/app/exercises/mvExerciseDataService.js
@@ -1,20 +1,25 @@
 (function() {
   angular
     .module('app')
-    .factory('mvExerciseDataService', function(mvScores) {
-      var service = {};
+    .factory('mvExerciseDataService', mvExerciseDataService);
 
-      service.setData = function(dataHash) {
+    function mvExerciseDataService(mvScores) {
+      var service = {
+        setData: setData,
+        save: save
+      };
+      return service;
+
+      function setData(dataHash) {
         if (dataHash.wpm) {service.wpm = dataHash.wpm; };
         if (dataHash.errorPercentage) { service.errorPercentage = dataHash.errorPercentage; };
         if (dataHash.wastedKeyStrokes) { service.wastedKeyStrokes = dataHash.wastedKeyStrokes; };
       };
 
-      service.save = function(dataHash) {
+      function save(dataHash) {
         console.log(dataHash);
         mvScores.save({score: dataHash});
       }
 
-      return service;
-    });
+    }
 })();
