f8abac88443c61212710e22103e8c07eacf67c95
gregfedirko <gfedirko@zagmail.gonzaga.edu>
2015-01-01 22:33:31 -0800

    move filters to /common

--- public/app/common/mvFilters.js
+++ public/app/common/mvFilters.js
@@ -1,20 +1,24 @@
 (function() {
   angular
     .module('app')
-    .filter('percentage', ['$filter', function($filter) {
+    .filter('percentage', percentage);
+
+    function percentage($filter) {
       return function(input, decimals) {
         if (!input) {
           return null
         }
         return $filter('number')(input * 100, decimals) + ' %';
       }
-    }]);
+    }
 })();
 
 (function() {
   angular
     .module('app')
-      .filter('formatTimer', function() {
+      .filter('formatTimer', formatTimer)
+
+      function formatTimer() {
         return function(input)
           {
             function z(n) {return (n<10? '0' : '') + n;}
@@ -23,5 +27,5 @@
             var hours = Math.floor(minutes / 60);
             return (z(minutes)+':'+z(seconds));
           };
-      });
+      }
 })();
